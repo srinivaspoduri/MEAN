ca4b7d39a0cf91e0788cbac4472f11ca6c1b8e8d
= <=>
2020-11-18 23:54:31 +0530

    removed .dist from git ignore

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/Ang-Routes/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+// Start the app by listening on the default Heroku port  .....
 app.listen(process.env.PORT || 8080);
