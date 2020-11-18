3fc998069a0de58b6c302181a3d19022e087850d
= <=>
2020-11-18 23:33:39 +0530

    updated

--- server.js
+++ server.js
@@ -8,10 +8,9 @@ const app = express();
 app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
 app.get('/*', function(req,res) {
-    console.log(__dirname)
-//res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-res.sendFile( path.resolve('src', 'app', 'index.html') );
+    
+res.sendFile(path.join(__dirname+'/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 1234);
+app.listen(process.env.PORT || 5000);
