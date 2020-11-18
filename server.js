38779c2a858a35d896c7cab6572eacd2a5a690fb
= <=>
2020-11-18 23:44:46 +0530

    added dist folder

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/src/index.html'));
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8080);
