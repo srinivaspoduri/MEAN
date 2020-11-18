b078853e55722c5848d9e55dd5dc613c0e21e9fe
= <=>
2020-11-18 22:31:20 +0530

    server.js moved to app folder

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const app = express();
 app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
 app.get('/*', function(req,res) {
-    
+    console.log(__dirname)
 res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 1234);
