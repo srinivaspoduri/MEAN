458107ed2d0df0fdef34450ea355fa989ccbc871
srinivaspoduri <srinu9tech@gmail.com>
2020-12-01 00:34:28 +0530

    Updated Search results view

--- src/app/pipes/currencypipe.ts
+++ src/app/pipes/currencypipe.ts
@@ -4,6 +4,6 @@ name: 'space'
 })
 export class SpacePipe implements PipeTransform {
 transform(value: any, args?: any): any {
-return '' + value.substring(0, 1) + ' ' + value.substring( 1);
+return ' ' + value.substring(0, 1) + ' ' + value.substring( 1);
 }
 }
